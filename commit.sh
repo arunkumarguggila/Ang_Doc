fcf4b7cabb0e7681e03f5a4f2d69768898478f84
arunkumarguggila <arunkumarguggila@gmail.com>
2023-12-25 21:13:59 +0400

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,23 +1,32 @@
+#!/bin/bash
+
 AZUREPAT=$AZUREPAT
 AZUSERNAME=$AZUSERNAME
 AZUSER_EMAIL=$AZUSER_EMAIL
 AZORG=$AZORG
+
+# Clone GitHub repository
 git clone https://github.com/arunkumarguggila/Ang_Doc
 cd Ang_Doc
 
+# Navigate back to the parent directory
 cd ..
 
+# Clone Azure DevOps repository
 GIT_CMD_REPOSITORY="https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/_git/GItLad_test"
 git clone $GIT_CMD_REPOSITORY
 
+# Copy files from GitHub to Azure DevOps
 cp -r Ang_Doc/* GItLad_test/
 
+# Navigate to the Azure DevOps repository
 cd GItLad_test
 
+# Configure Git user information
 git config --global user.email "$AZUSER_EMAIL"
 git config --global user.name "$AZUSERNAME"
 
+# Commit and push changes
 git add .
-git commit -m "sync from git to azure"
-
+git commit -m "Sync from GitHub to Azure DevOps"
 git push
